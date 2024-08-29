e946648e3e7aa8450a13a13452f250b2595e1ffd
Sofyan Sauri <sofyan.sauri0798@gmail.com>
2024-08-30 01:22:38 +0700

    baru 1

--- src/main/java/com/juaracoding/coba2.java
+++ src/main/java/com/juaracoding/coba2.java
@@ -11,18 +11,76 @@ Created on 30/08/2024 01:02
 @Last Modified 30/08/2024 01:02
 Version 1.0
 */
+import java.util.Scanner;
 
 public class coba2 {
-    public static void main (String[] args){
+    public static void main (String[] args) {
+
+        System.out.print("Hallo word");
         float f = 3.324243532f;
         double d = 3.23442323445242;
-        System.out.println("Default value f = "+f);
-        System.out.println("Default value d = "+d);
+        System.out.println("Default value f = " + f);
+        System.out.println("Default value d = " + d);
 
         d = f;
 
-        System.out.println("Default value f (long)    = "+f);
-        System.out.println("Default value d (float)   = "+d);
+        System.out.println("Default value f (long)    = " + f);
+        System.out.println("Default value d (float)   = " + d);
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Simple Calculator");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+
+        System.out.print("Enter your choice (1-4): ");
+        int choice = scanner.nextInt();
+        System.out.print("Enter first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = scanner.nextDouble();
+        switch (choice) {
+            case 1:
+                System.out.println("Result: " + add(num1, num2));
+                break;
+            case 2:
+                System.out.println("Result: " + subtract(num1, num2));
+                break;
+            case 3:
+                System.out.println("Result: " + multiply(num1, num2));
+                break;
+            case 4:
+                if (num2 != 0) {
+                    System.out.println("Result: " + divide(num1, num2));
+                } else {
+                    System.out.println("Error! Division by zero is not allowed.");
+                }
+                break;
+            default:
+                System.out.println("Invalid choice");
+
+        }
+        scanner.close();
     }
 
+    public static double add(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public static double subtract(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public static double multiply(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    public static double divide(double num1, double num2) {
+        return num1 / num2;
+    }
 }
+
