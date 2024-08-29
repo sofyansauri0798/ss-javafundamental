5b7c8d6d8cfbec47b7a78ef01368efd309761325
Sofyan Sauri <sofyan.sauri0798@gmail.com>
2024-08-30 00:26:12 +0700

    baru 1

---
+++ src/main/java/com/juaracoding/coba2.java
@@ -0,0 +1,28 @@
+package com.juaracoding;
+
+
+
+/*
+IntelliJ IDEA 2022.3.1 (Community Edition)
+Build #IC-223.8214.52, built on August 19, 2024
+@Author Sofyan Sauri a.k.a. Sofyan Sauri
+Java Developer
+Created on 30/08/2024 01:02
+@Last Modified 30/08/2024 01:02
+Version 1.0
+*/
+
+public class coba2 {
+    public static void main (String[] args){
+        float f = 3.324243532f;
+        double d = 3.23442323445242;
+        System.out.println("Default value f = "+f);
+        System.out.println("Default value d = "+d);
+
+        d = f;
+
+        System.out.println("Default value f (long)    = "+f);
+        System.out.println("Default value d (float)   = "+d);
+    }
+
+}
