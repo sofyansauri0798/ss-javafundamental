99c49f22597f10c9b65dcb90b12bb98052fde0b8
Sofyan Sauri <sofyan.sauri0798@gmail.com>
2024-08-30 11:46:07 +0700

    baru 4.2

---
+++ src/main/java/com/juaracoding/sof.java
@@ -0,0 +1,16 @@
+package com.juaracoding;
+
+
+
+/*
+IntelliJ IDEA 2022.3.1 (Community Edition)
+Build #IC-223.8214.52, built on August 19, 2024
+@Author Sofyan Sauri a.k.a. Sofyan Sauri
+Java Developer
+Created on 30/08/2024 11:34
+@Last Modified 30/08/2024 11:34
+Version 1.0
+*/
+
+public class sof {
+}
