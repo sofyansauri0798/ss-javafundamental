066061ce19ea7e1ae01531119ad99b53af38faaa
Sofyan Sauri <sofyan.sauri0798@gmail.com>
2024-09-01 07:31:29 +0700

    coba database

--- src/main/java/com/juaracoding/trial.java
+++ src/main/java/com/juaracoding/trial.java
@@ -14,7 +14,25 @@ Version 1.0
 
 public class trial {
     public static void main(String[] args){
-        System.out.print("Hallo kamu");
-
+        short dataFirst = 457;
+        double doubleData = dataFirst; //short ke double
+        char dataSecond = 'W';
+        long longData = dataSecond;//char ke long
+        int dataThird = 456;
+        float floatData = dataThird;// int ke float
+        byte dataFourth = 127;
+        long longDataSecond = dataFourth;//byte ke long
+        float dataFifth = 565.3f;
+        double doubleDataSecond = dataFifth;//float ke double
+        System.out.println("Short ke Double: "+ doubleData);
+        System.out.println("Char ke Long: "+ longData);
+        System.out.println("Int ke Float: "+ floatData);
+        System.out.println("Byte ke Long: "+ longDataSecond);
+        System.out.println("Float ke Double: "+ doubleDataSecond);
+int x =10;
+        System.out.println(x);
+        x=12;
+        System.out.println(x);
     }
 }
+
